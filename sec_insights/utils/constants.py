980495aba120038be6bb0e308c04a013a8d37dab
Ajinkya Nawarkar <anawarkar@ConverseMac.local>
2024-11-25 16:02:50 -0500

    add master ticker list and add form arg

---
+++ sec_insights/utils/constants.py
@@ -0,0 +1,5 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+ASSETS_DIR = os.path.join(BASE_DIR, "assets")
+MASTER_CONFIG = os.path.join(BASE_DIR, "tickers_config.json")
