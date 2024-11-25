9203845385a878cd10b7122a7a57f858b505185d
mallik mahalingam <mallik.mahalingam@gmail.com>
2024-11-24 23:59:00 -0800

    Modified code to directly use pdfkit instead of 2 staged operation. Plus now images work.

--- src/pull.py
+++ src/pull.py
@@ -31,11 +31,12 @@ print(f"Count: {args.count}")
 if args.count < 2:
    parser.error("# of items to process must be > 2")
 
-user_agent = "Fintwit Developer (fintwit.dev@gmail.com)"
 set_identity("fintwit.dev@gmail.com")
+user_agent = "Fintwit Developer (fintwit.dev@gmail.com)"
 headers = { 'User-Agent': user_agent }
-ticker = args.ticker
+cheader = { 'custom-header': [('User-Agent', user_agent)], }
 
+ticker = args.ticker
 c = Company(ticker)
 filings = c.get_filings(form="8-K").latest(args.count)
 #print(filings.company_name, filings.cik)
@@ -50,22 +51,11 @@ for f in filings:
     #print(exhibits)
     results = exhibits.query("document_type in ['EX-99.1', 'EX-99.01']")
     for r in results:
-      print("Filing Date = ",f.filing_date, r.url)
-      response = requests.get(r.url, headers=headers)
-      #print(response.status_code)
-      if response.status_code == 200 :
-      	#print(response.text)
-      	fname = ticker + "_" + str(f.filing_date)
-      	with open(fname + ".htm","w") as f:
-                f.write(response.text)
-                infile = fname +'.htm'
-                outfile = fname +'.pdf'
-                print("converting ", infile, "to ", outfile)
-                try:
-                    pdfkit.from_file(infile, outfile)
-                    print("converted ", infile, "to ", outfile,"\n")
-                except OSError as e:
-                    print("Encountered an error in converting ", infile, "to ", outfile,"\n")
-                except Exception as e:
-                    print("Encountered an exception in converting ", infile, "to ", outfile,"\n")
-                os.remove(infile)
+       fname = ticker + "_" + str(f.filing_date) + ".pdf"
+       try:
+          print("Fetching",r.url, fname)
+          pdfkit.from_url(r.url, fname, options=cheader)
+       except OSError as e:
+          print("Encountered error in Fetching", r.url)
+       except Exception as e:
+          print("Encountered an exception in Fetching", r.url)
